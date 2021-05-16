d272b18a8246020b2ba50a20bdf5f439c1e7718f
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2021-05-16 11:10:05 +0300

    Add files via upload

---
+++ Library/Library/Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Library
+{
+    public class Library
+    {
+        public static void math()
+        {
+            unsafe
+            {
+                public int Sum(int a, int b)
+                {
+                    return a + b;
+                }
+                public int Subtraction(int a, int b)
+                {
+                    return a - b;
+                }
+                public int Multiply(int a, int b)
+                {
+                    return a * b;
+                }
+                public int Divide(int a, int b)
+                {
+                    return a / b;
+                }
+                public int Mod(int a, int b)
+                {
+                    return a % b;
+                }
+            }
+        }
+    }
+}
