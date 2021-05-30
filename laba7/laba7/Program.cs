33ece13001bbbf6d2234cafc583f483c2b2f605b
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2021-05-30 21:59:41 +0300

    laba7

---
+++ laba7/laba7/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Globalization;
+namespace laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nRational number: \n");
+            RationalNumber r1 = new RationalNumber(15, 10);
+            Console.WriteLine(r1);
+            RationalNumber r2 = new RationalNumber(7, 3);
+            Console.WriteLine(r2);
+            Console.WriteLine("\nMathematical operations : \n");
+            RationalNumber sum = r1 + r2;
+            Console.WriteLine($"{r1}+{r2} = {sum}");
+            RationalNumber sub = r1 - r2;
+            Console.WriteLine($"{r1}-{r2} = {sub}");
+            RationalNumber mult = r1 * r2;
+            Console.WriteLine($"{r1}*{r2} = {mult}");
+            RationalNumber div = r1 / r2;
+            Console.WriteLine($"{r1}/{r2} = {div}");
+            Console.WriteLine("\nComparison operations: \n");
+            Console.WriteLine($"{r1} > {r2} - {r1 > r2}");
+            Console.WriteLine($"{r1} < {r2} - {r1 < r2}");
+            Console.WriteLine($"{r1} >= {r2} - {r1 >= r2}");
+            Console.WriteLine($"{r1} <= {r2} - {r1 <= r2}");
+            Console.WriteLine($"{r1} = {r2} - {r1 == r2}");
+            Console.WriteLine($"{r1} != {r2} - {r1 != r2}\n");
+            Console.WriteLine(sum.GetHashCode());
+            Console.WriteLine("\nReduce method test : \n");
+            RationalNumber red = sum.Reduce();
+            Console.WriteLine(red);
+            Console.WriteLine("\nConvert methods tests : \n");
+            var provider = CultureInfo.CurrentCulture;
+            TypeCode typeCode = r1.GetTypeCode();
+            Console.WriteLine($"Type - {typeCode}");
+            bool toBool = r1.ToBoolean(provider);
+            Console.WriteLine($"Bool - {toBool}");
+            byte toByte = r1.ToByte(provider);
+            Console.WriteLine($"Byte - {toByte}");
+            char toChar = r1.ToChar(provider);
+            Console.WriteLine($"Char - {toChar}");
+            DateTime toDT = r1.ToDateTime(provider);
+            Console.WriteLine($"Data - {toDT}");
+            decimal toDecimal = r1.ToDecimal(provider);
+            Console.WriteLine($"Decimal - {toDecimal}");
+            double toDouble = r1.ToDouble(provider);
+            Console.WriteLine($"Double - {toDouble}");
+            Int16 toInt16 = r1.ToInt16(provider);
+            Console.WriteLine($"Int16 - {toInt16}");
+            Int32 toInt32 = r1.ToInt32(provider);
+            Console.WriteLine($"Int32 - {toInt32}");
+            Int64 toInt64 = r1.ToInt64(provider);
+            Console.WriteLine($"Int64 - {toInt64}");
+            SByte toSByte = r1.ToSByte(provider);
+            Console.WriteLine(toSByte);
+            Single toSingle = r1.ToSingle(provider);
+            Console.WriteLine(toSingle);
+            UInt16 toUInt16 = r1.ToUInt16(provider);
+            Console.WriteLine(toUInt16);
+            UInt32 toUInt32 = r1.ToUInt32(provider);
+            Console.WriteLine(toUInt32);
+            UInt64 toUInt64 = r1.ToUInt64(provider);
+            Console.WriteLine(toUInt64);
+            ulong toType = (ulong)r1.ToType(typeof(ulong), provider);
+            Console.WriteLine(toType);
+            Console.WriteLine("\nConvert operators : \n");
+            Console.WriteLine($"Int - {(int)r1}");
+            Console.WriteLine($"Long - {(long)r1}");
+            Console.WriteLine($"Decimal - {(decimal)r1}");
+            Console.WriteLine($"Double - {(double)r1}");
+            Console.WriteLine($"Float - {(float)r1}");
+            int testInt = 10;
+            RationalNumber testIntRn = testInt;
+            Console.WriteLine(testIntRn);
+            long testLong = 10;
+            RationalNumber testLongRn = testLong;
+            Console.WriteLine(testLongRn);
+            decimal testDecimal = 10.2m;
+            RationalNumber testDecimalRn = testDecimal;
+            Console.WriteLine(testDecimalRn);
+            double testDouble = 10.2;
+            RationalNumber testDoubleRn = testDouble;
+            Console.WriteLine(testDoubleRn);
+            float testFloat = 10.2f;
+            RationalNumber testFloatRn = testFloat;
+            Console.WriteLine(testFloatRn);
+            Console.WriteLine("\nFormat tests : \n");
+            Console.WriteLine(r1.ToString("F"));
+            Console.WriteLine(r1.ToString("D"));
+            Console.WriteLine(r1.ToString("P"));
+            Console.WriteLine(r1.ToString("N"));
+            try
+            {
+                Console.WriteLine(r1.ToString("None"));
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
---
+++ laba7/laba7/RationalNumber.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Globalization;
+namespace laba7
+{
+    class RationalNumber : IEquatable<RationalNumber>, IComparable<RationalNumber>, IConvertible, IFormattable
+    {
+        private int Numerator { get; }
+        private int Denominator { get; }
+        public RationalNumber(int Numerator, int Denominator)
+        {
+            this.Numerator = Numerator;
+            this.Denominator = (Denominator > 0) ? Denominator : 1;
+        }
+        public static RationalNumber operator +(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Add(r2);
+        }
+        public static RationalNumber operator -(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Subtract(r2);
+        }
+        public static RationalNumber operator *(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Multiply(r2);
+        }
+        public static RationalNumber operator /(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Divide(r2);
+        }
+        public static bool operator ==(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.Equals(r2);
+        }
+        public static bool operator !=(RationalNumber r1, RationalNumber r2)
+        {
+            return !r1.Equals(r2);
+        }
+        public static bool operator <(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.CompareTo(r2) < 0;
+        }
+        public static bool operator >(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.CompareTo(r2) > 0;
+        }
+        public static bool operator <=(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.CompareTo(r2) <= 0;
+        }
+        public static bool operator >=(RationalNumber r1, RationalNumber r2)
+        {
+            return r1.CompareTo(r2) >= 0;
+        }
+        public RationalNumber Add(RationalNumber other)
+        {
+            int numerator = (Numerator * other.Denominator) + (other.Numerator * Denominator);
+            int denominator = Denominator * other.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+        public RationalNumber Subtract(RationalNumber other)
+        {
+            int numerator = (Numerator * other.Denominator) - (other.Numerator * Denominator);
+            int denominator = Denominator * other.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+        public RationalNumber Multiply(RationalNumber other)
+        {
+            int numerator = Numerator * other.Numerator;
+            int denominator = Denominator * other.Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+        public RationalNumber Divide(RationalNumber other)
+        {
+            int numerator = Numerator * other.Denominator;
+            int denominator = (other.Numerator == 0)
+                ? throw new DivideByZeroException()
+                : other.Numerator * Denominator;
+            return new RationalNumber(numerator, denominator);
+        }
+        public bool Equals(RationalNumber other)
+        {
+            return this.GetDoubleValue() == other.GetDoubleValue();
+        }
+        public override bool Equals(object o)
+        {
+            RationalNumber rn = o as RationalNumber;
+            return Equals(rn);
+        }
+        public int CompareTo(RationalNumber other)
+        {
+            double thisNumber = this.GetDoubleValue();
+            double otherNumber = other.GetDoubleValue();
+
+            if (thisNumber > otherNumber)
+                return 1;
+            else if (thisNumber < otherNumber)
+                return -1;
+            else
+                return 0;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Numerator, this.Denominator);
+        }
+        public RationalNumber Reduce()
+        {
+            int numb = GreatestCommonDivisor(this.Numerator, this.Denominator);
+            int numerator = this.Numerator / numb;
+            int denominator = this.Denominator / numb;
+            if (denominator < 0)
+            {
+                return new RationalNumber(-numerator, -denominator);
+            }
+            return new RationalNumber(numerator, denominator);
+        }
+        public static implicit operator RationalNumber(int value)
+        {
+            return new RationalNumber(value, 1);
+        }
+        public static implicit operator RationalNumber(long value)
+        {
+            return new RationalNumber((int)value, 1);
+        }
+        public static implicit operator RationalNumber(decimal value)
+        {
+            return CreateFromDouble((double)value);
+        }
+        public static implicit operator RationalNumber(double value)
+        {
+            return CreateFromDouble(value);
+        }
+        public static implicit operator RationalNumber(float value)
+        {
+            decimal dec = new decimal(value);
+            return CreateFromDouble((double)dec);
+        }
+        public static explicit operator int(RationalNumber value)
+        {
+            return (int)value.Numerator / value.Denominator;
+        }
+        public static explicit operator long(RationalNumber value)
+        {
+            return (long)value.Numerator / value.Denominator;
+        }
+        public static explicit operator decimal(RationalNumber value)
+        {
+            return (decimal)value.Numerator / value.Denominator;
+        }
+        public static explicit operator double(RationalNumber value)
+        {
+            return (double)value.Numerator / value.Denominator;
+        }
+        public static explicit operator float(RationalNumber value)
+        {
+            return (float)value.Numerator / value.Denominator;
+        }
+        public TypeCode GetTypeCode()
+        {
+            return TypeCode.Object;
+        }
+        public bool ToBoolean(IFormatProvider provider)
+        {
+            return this.Numerator != 0;
+        }
+        public byte ToByte(IFormatProvider provider)
+        {
+            return Convert.ToByte(GetDoubleValue());
+        }
+        public char ToChar(IFormatProvider provider)
+        {
+            return Convert.ToChar(ToString(provider)[0]);
+        }
+        public DateTime ToDateTime(IFormatProvider provider)
+        {
+            return DateTime.Now;
+        }
+        public decimal ToDecimal(IFormatProvider provider)
+        {
+            return Convert.ToDecimal(GetDoubleValue());
+        }
+        public double ToDouble(IFormatProvider provider)
+        {
+            return GetDoubleValue();
+        }
+        public short ToInt16(IFormatProvider provider)
+        {
+            return Convert.ToInt16(GetDoubleValue());
+        }
+        public int ToInt32(IFormatProvider provider)
+        {
+            return Convert.ToInt32(GetDoubleValue());
+        }
+        public long ToInt64(IFormatProvider provider)
+        {
+            return Convert.ToInt64(GetDoubleValue());
+        }
+        public sbyte ToSByte(IFormatProvider provider)
+        {
+            return Convert.ToSByte(GetDoubleValue());
+        }
+        public float ToSingle(IFormatProvider provider)
+        {
+            return Convert.ToSingle(GetDoubleValue());
+        }
+        public string ToString(IFormatProvider provider)
+        {
+            return this.ToString("F", CultureInfo.CurrentCulture);
+        }
+        public object ToType(Type conversionType, IFormatProvider provider)
+        {
+            return Convert.ChangeType(GetDoubleValue(), conversionType);
+        }
+        public ushort ToUInt16(IFormatProvider provider)
+        {
+            return Convert.ToUInt16(GetDoubleValue());
+        }
+        public uint ToUInt32(IFormatProvider provider)
+        {
+            return Convert.ToUInt32(GetDoubleValue());
+        }
+        public ulong ToUInt64(IFormatProvider provider)
+        {
+            return Convert.ToUInt64(GetDoubleValue());
+        }
+        public string ToString(string format)
+        {
+            return this.ToString(format, CultureInfo.CurrentCulture);
+        }
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            if (String.IsNullOrEmpty(format)) format = "F";
+            if (formatProvider == null) formatProvider = CultureInfo.CurrentCulture;
+            switch (format.ToUpperInvariant())
+            {
+                case "F":
+                    return $"{this.Numerator}/{this.Denominator}";
+                case "D":
+                    return GetDoubleValue().ToString();
+                case "P":
+                    return GetDoubleValue().ToString("P", formatProvider);
+                case "N":
+                    return GetDoubleValue().ToString("N", formatProvider);
+                default:
+                    throw new FormatException($"The {format} format string is not supported.");
+            }
+        }
+        private double GetDoubleValue()
+        {
+            return this.Numerator / this.Denominator;
+        }
+        private int GreatestCommonDivisor(int a, int b)
+        {
+            return b == 0 ? Math.Abs(a) : GreatestCommonDivisor(Math.Abs(b), Math.Abs(a) % Math.Abs(b));
+        }
+        private static RationalNumber CreateFromDouble(double doubleNumber)
+        {
+            string number = doubleNumber.ToString();
+            try
+            {
+                int intPart = Convert.ToInt32(number.Substring(0, number.IndexOf(",")));
+                double doublePart = Convert.ToDouble("0" + number.Substring(number.IndexOf(",") - 1, number.Length - 1));
+                int denominator = (int)Math.Pow(10, doublePart.ToString().Length - 2);
+                doublePart *= denominator;
+                int numerator = (int)(intPart * denominator + doublePart);
+                return new RationalNumber(numerator, denominator);
+            }
+            catch (Exception)
+            {
+                return new RationalNumber((int)doubleNumber, 1);
+            }
+        }
+    }
+}
