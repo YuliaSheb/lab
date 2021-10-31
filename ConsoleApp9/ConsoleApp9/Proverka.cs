d4e6141053bff9e8850114818501a8d4973fb450
AnatolShebeko <46474868+AnatolShebeko@users.noreply.github.com>
2021-10-31 16:32:38 +0100

    Change code

--- ConsoleApp9/ConsoleApp9/Proverka.cs
+++ ConsoleApp9/ConsoleApp9/Proverka.cs
@@ -35,4 +35,61 @@ namespace ConsoleApp9
             }
         }
     }
+    /*
+        public class Proverka
+    {
+		private List<Person> cut_queue = new List<Person>();
+		private List<Person> processed = new List<Person> ();
+		private TimeSpan Time_to_cut = new TimeSpan(00, 40, 00);
+		/// <summary>
+		/// Check queue and returns its current size
+		/// </summary>
+		/// <param name="current_moment"></param>
+		/// <returns></returns>
+		private int CheckQueueSize(DateTime current_moment) {
+		    foreach(var p in cut_queue) {
+				if (p.End_date < current_moment) {
+					// Mark that person is cutted and add to list of processed persons
+					p.IsCutted = true;
+					processed.Add(p);
+					// Remove person who is processed from queue 
+					cut_queue.Remove(p);
+				}
+			}
+			return cut_queue.Count;
+		}
+		
+        public Proverka(List<Person> list) {
+			TimeSpan time_to_cut = new TimeSpan(00, 40, 00);
+			foreach (Person l in list)
+			{
+				// If person do not like to wait
+				if (CheckQueueSize(l.Start_date) > l.Terpenie)
+				{
+					l.IsCutted = false;
+				}
+				// If person will wait
+				else
+				{
+					if (cut_queue.Count > 0)
+					{
+						var End_date_to_last = cut_queue.Last().End_date;
+						if (End_date_to_last > l.Start_date)
+						{
+							l.End_date = End_date_to_last + Time_to_cut;
+						} else
+                        {
+							l.End_date = l.Start_date + Time_to_cut;
+						}
+					}
+					else
+					{
+						l.End_date = l.Start_date + Time_to_cut;
+					}
+					cut_queue.Add(l);
+				}
+			}
+		}
+    }
+    */
 }
