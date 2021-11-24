f0d678c1b1296843259035c8bc7113a7c4780cc4
YuliaSheb <79199794+YuliaSheb@users.noreply.github.com>
2021-11-24 10:04:11 +0300

    Add files via upload

--- Ferma/Ferma/Program.cs
+++ Ferma/Ferma/Program.cs
@@ -11,32 +11,38 @@ namespace Ferma
         enum Tenants
         {
             People = 3, 
-            Chiken = 5,
-            Duck = 4,
-            Cow = 8,
-            Dog = 6,
-            Horse = 10,
+            Chiken = 2,
+            Duck = 2,
+            Cow = 4,
+            Dog = 3,
+            Horse = 5,
             Endurence = 5      
         }
-        static void Opred()
+        static void Opred(int value_people, int value_chiken,int  value_duck, int value_cow, int value_dog, int value_horse)
         {
             People people = new People((int)Tenants.People);
+            People[] p = new People[value_people];
             Console.WriteLine(people);
             Chikens chikens = new Chikens((int)Tenants.Chiken);
+            Chikens[] ch = new Chikens[value_chiken];
             Console.WriteLine(chikens);
             Ducks ducks = new Ducks((int)Tenants.Duck);
+            Ducks[] du = new Ducks[value_duck];
             Console.WriteLine(ducks);
             Cows cows = new Cows((int)Tenants.Cow);
+            Cows[] c = new Cows[value_cow];
             Console.WriteLine(cows);
             Dogs dogs = new Dogs((int)Tenants.Dog);
+            Dogs[] d = new Dogs[value_dog];
             Console.WriteLine(dogs);
             Horses horses = new Horses((int)Tenants.Horse, (int)Tenants.Endurence);
+            Horses[] h = new Horses[value_horse];
             Console.WriteLine(horses);
         }
-        static int Calc()
+        static int Calc(int value_people, int value_chiken, int value_duck, int value_cow, int value_dog, int value_horse)
         {
             int sum = 0;
-            sum = (int)Tenants.People + (int)Tenants.Chiken + (int)Tenants.Duck + (int)Tenants.Cow + (int)Tenants.Dog + (int)Tenants.Horse;
+            sum = value_people*(int)Tenants.People + value_chiken*(int)Tenants.Chiken + value_duck * (int)Tenants.Duck + value_cow * (int)Tenants.Cow + value_dog * (int)Tenants.Dog + value_horse * (int)Tenants.Horse;
             int k = 1;
             while ((int)Tenants.Endurence < sum)
             {
@@ -48,8 +54,9 @@ namespace Ferma
         }
         static void Main(string[] args)
         {
-            Opred();
-            Calc();
+            int value_people = 30, value_chiken = 15, value_duck = 7, value_cow = 10, value_dog = 5, value_horse = 25;
+            Opred(value_people, value_chiken , value_duck , value_cow , value_dog, value_horse );
+            Calc(value_people, value_chiken, value_duck, value_cow, value_dog, value_horse);
             Console.ReadKey();
         }
     }
